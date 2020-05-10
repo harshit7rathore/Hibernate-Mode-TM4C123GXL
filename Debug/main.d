aa7fb0a01102aa29d8d0606c27ebf97a1b042c1d
HARSHIT RATHORE <64119793+harshit7rathore@users.noreply.github.com>
2020-05-10 10:28:34 +0530

    Code for Hibernate Mode on TM4C123GXL

---
+++ Debug/main.d
@@ -0,0 +1,67 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Users/harshitrathore/Documents/Texas/TIVA//\ C123GH/Hibernate/h.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/sysctl.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/gpio.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/timer.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/interrupt.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/adc.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/pwm.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/pin_map.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/uart.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/debug.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/hibernate.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/inc/hw_memmap.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/inc/hw_types.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/inc/hw_gpio.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/inc/tm4c123gh6pm.h
+main.obj: /Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/utils/ustdlib.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/time.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_ti_config.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/linkage.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/abi_prefix.h
+main.obj: /Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/xlocale/_time.h
+
+../main.c: 
+/Users/harshitrathore/Documents/Texas/TIVA//\ C123GH/Hibernate/h.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/sysctl.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/gpio.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/timer.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/interrupt.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/adc.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/pwm.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/pin_map.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/uart.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/debug.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/driverlib/hibernate.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/inc/hw_memmap.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/inc/hw_types.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/inc/hw_gpio.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/inc/tm4c123gh6pm.h: 
+/Applications/ti/ccs901/TivaWare_C_Series-2.1.2.111/utils/ustdlib.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/time.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_ti_config.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/linkage.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/abi_prefix.h: 
+/Applications/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/xlocale/_time.h: 
---
+++ h.h
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/adc.h"
+#include "driverlib/pwm.h"
+#include "driverlib/pin_map.h"
+#include"driverlib/uart.h"
+#include "driverlib/debug.h"
+#include "driverlib/hibernate.h"
+
+
+
+#include <inc/hw_memmap.h>
+#include <inc/hw_types.h>
+#include <inc/hw_gpio.h>
+#include <inc/tm4c123gh6pm.h>
+
+
+
+#include "utils/ustdlib.h"
